b7588f10d35d16ba9e0077f7565c3d6d6c8193f2
fcucullu <francisco.cucullu@gmail.com>
2024-07-28 13:31:24 +0200

    Add django-heroku configuration

--- financeproject/settings.py
+++ financeproject/settings.py
@@ -14,6 +14,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
