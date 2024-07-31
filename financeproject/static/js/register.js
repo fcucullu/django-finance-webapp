9d74d09aa2e247cccb06afdee57511eb0b164a2d
fcucullu <francisco.cucullu@gmail.com>
2024-07-31 17:55:34 +0200

    add password hide/show functionality

--- financeproject/static/js/register.js
+++ financeproject/static/js/register.js
@@ -1,18 +1,32 @@
 const usernameField = document.querySelector("#usernameField");
-const usernameFeedBackArea = document.querySelector(".username_invalid_feedback");
+const usernameFeedBackArea = document.querySelector(
+  ".username_invalid_feedback"
+);
 const emailField = document.querySelector("#emailField");
 const emailFeedBackArea = document.querySelector(".email_invalid_feedback");
-const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput');
-const emailSuccessOutput = document.querySelector('.emailSuccessOutput');
+const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
+const emailSuccessOutput = document.querySelector(".emailSuccessOutput");
+const passwordField = document.querySelector("#passwordField");
+const showPasswordToggle = document.querySelector(".showPasswordToggle");
 
+const handleToggleInput = (e) => {
+  if (showPasswordToggle.textContent === "SHOW") {
+    showPasswordToggle.textContent = "HIDE";
+    passwordField.setAttribute("type", "text");
+  } else {
+    showPasswordToggle.textContent = "SHOW";
+    passwordField.setAttribute("type", "password");
+  }
+};
 
-emailField.addEventListener("keyup", (e) => {
+showPasswordToggle.addEventListener("click", handleToggleInput),
+  emailField.addEventListener("keyup", (e) => {
     const emailVal = e.target.value;
-    emailSuccessOutput.textContent=`Checking ${emailVal}...`;
+    emailSuccessOutput.textContent = `Checking ${emailVal}...`;
 
     emailField.classList.remove("is-invalid");
     emailFeedBackArea.style.display = "none";
-  
+
     if (emailVal.length > 0) {
       fetch("/authentication/validate-email", {
         body: JSON.stringify({ email: emailVal }),
@@ -21,7 +35,7 @@ emailField.addEventListener("keyup", (e) => {
         .then((res) => res.json())
         .then((data) => {
           console.log("data", data);
-          emailSuccessOutput.style.display='none'
+          emailSuccessOutput.style.display = "none";
           if (data.email_error) {
             emailField.classList.add("is-invalid");
             emailFeedBackArea.style.display = "block";
@@ -33,7 +47,7 @@ emailField.addEventListener("keyup", (e) => {
 
 usernameField.addEventListener("keyup", (e) => {
   const usernameVal = e.target.value;
-  usernameSuccessOutput.textContent=`Checking ${usernameVal}...`;
+  usernameSuccessOutput.textContent = `Checking ${usernameVal}...`;
 
   usernameField.classList.remove("is-invalid");
   usernameFeedBackArea.style.display = "none";
@@ -46,7 +60,7 @@ usernameField.addEventListener("keyup", (e) => {
       .then((res) => res.json())
       .then((data) => {
         console.log("data", data);
-        usernameSuccessOutput.style.display='none'
+        usernameSuccessOutput.style.display = "none";
         if (data.username_error) {
           usernameField.classList.add("is-invalid");
           usernameFeedBackArea.style.display = "block";
