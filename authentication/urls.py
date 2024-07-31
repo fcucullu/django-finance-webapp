0c08dc2cf10838ed4d0dbf5533e9a6c6a7892f0c
fcucullu <francisco.cucullu@gmail.com>
2024-07-31 16:28:33 +0200

    add errors in username and email when wrong input

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, UsernameValidationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login', RegistrationView.as_view(), name='login'),
     path('reset-password', RegistrationView.as_view(), name='reset-password'),
     path('set-newpassword', RegistrationView.as_view(), name='set-newpassword'),
-    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username")
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
+    path('validate-email',csrf_exempt(EmailValidationView.as_view()), name='validate-email'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,20 @@ from django.views import View
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 import json
+from validate_email import validate_email
 
+class EmailValidationView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error':'Email is invalid'}, status=400)
+    
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error':'Email is used, please choose another one'}, status=409)
+
+        return JsonResponse({'email_valid': True})
 
 
 class UsernameValidationView(View):
@@ -12,10 +25,10 @@ class UsernameValidationView(View):
         username = data['username']
 
         if not str(username).isalnum():
-            return JsonResponse({'username_error':'username should only contain alphanumeric characters'}, status=400)
+            return JsonResponse({'username_error':'Username should only contain alphanumeric characters'}, status=400)
     
         if User.objects.filter(username=username).exists():
-            return JsonResponse({'username_error':'username is used, please choose another one'}, status=409)
+            return JsonResponse({'username_error':'Username is used, please choose another one'}, status=409)
 
         return JsonResponse({'username_valid': True})
 
