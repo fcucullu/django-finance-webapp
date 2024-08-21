b82bd3b941ffe984cc97e5e124c2f1d8c7ae6607
fcucullu <francisco.cucullu@gmail.com>
2024-08-21 12:03:17 +0200

    update main

--- configuration/settings.py
+++ configuration/settings.py
@@ -147,7 +147,7 @@ MESSAGE_TAGS={
 
 
 #EMAIL CONFIGURATION
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
